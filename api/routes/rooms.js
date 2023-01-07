c4bc0dd6bff545d2cab92ec875153bbb98a9b596
hungngo04 <ngotienhung195@gmail.com>
2023-01-06 23:45:16 -0500

    room processing

---
+++ api/routes/rooms.js
@@ -0,0 +1,19 @@
+import express from "express";
+import { createRoom, deleteRoom, getRoom, getRooms, updateRoom } from "../controllers/Room.js";
+import Room from "../models/Room.js"
+import { verifyAdmin } from "../utils/verifyToken.js"
+
+const router = express.Router();
+
+//create
+router.post("/", verifyAdmin, createRoom);
+//update
+router.put("/:id", verifyAdmin, updateRoom)
+//delete
+router.delete("/:id", verifyAdmin, deleteRoom)
+//get
+router.get("/:id", getRoom)
+//get all
+router.get("/", getRooms)
+
+export default router;
